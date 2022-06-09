8fc1411c4b53463ea02fbf8ae272b4af95370348
saskiabt <sskbinder@gmail.com>
2022-06-08 22:21:17 -0400

    basic ui

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// function book(title,author,pages,isRead) { 
-//     this.title = title;
-//     this.author = author; 
-//     this.pages = pages; 
-//     this.isRead = isRead; 
-
-//     this.info = function() {
-//         if (isRead === true) { 
-//             return `${title} by ${author}, ${pages} pages, already read. `;
-//         } else {
-//             return `${title} by ${author}, ${pages} pages, not read yet `;
-//         }
-//     }
-// }
-
-// const theHobbit = new book('The Hobbit', "JRR Tolkien", "295", false); 
-
-// console.log(theHobbit.info()); 
-
 
 let myLibrary = []; 
 
@@ -44,52 +25,32 @@ function addBookToLibrary(book) {
 // display form on click of create book button
 const bookButton = document.getElementById("create-book-button"); 
 bookButton.addEventListener('click', () => {
+
     bookButton.style.visibility = "hidden"; 
     openForm(); 
 
-    document.querySelector("#popup-form > form > ul > button.cancel-btn").addEventListener('click', () => { 
+    document.querySelector("#list-item-5 > button.cancel-btn").addEventListener('click', () => { 
         closeForm();
         bookButton.style.visibility = "visible";
     });
+
+
+    function openForm() { 
+        document.getElementById('popup-form').style.display = "block"; 
+    }
+    function closeForm() { 
+        document.getElementById('popup-form').style.display = "none"; 
+    }
+ 
+    // CODE TO CLOSE POPUP WHEN CLICK ANYWHERE OUTSIDE POPUP BOX // 
+    // window.addEventListener('click', (event) => { 
+    //     let modal = document.getElementById('popup-form');
+    //     if (event.target == modal) {
+    //         closeForm();
+    //     }
+    // })
 });
 
-function openForm() { 
-    document.getElementById('popup-form').style.display = "block"; 
-}
-
-function closeForm() { 
-    document.getElementById('popup-form').style.display = "none"; 
-}
-    // let form = document.createElement('form'); 
-    //     let formBox = document.getElementById('form-box');
-    //     formBox.appendChild(form); 
-    //     form.setAttribute('method', "post"); 
-    //     form.setAttribute('action', 'submit.php'); 
-
-    //     function createForm(name) { 
-    //         const bullet = document.createElement('ul'); 
-    //         form.appendChild(bullet)
-    //         const label = document.createElement('label'); 
-    //         label.setAttribute('for', `${name}`)
-    //         label.innerHTML = `${name}`; 
-    //         bullet.appendChild(label); 
-        
-    //         let input = document.createElement('input'); 
-    //         bullet.appendChild(input); 
-    //         input.setAttribute('type', 'text'); 
-    //         input.setAttribute('id', `${name}`); 
-    //         input.setAttribute('name', `${name}`); 
-    //     }
-        
-    //     createForm('Title');
-    //     createForm('Author'); 
-    //     createForm('Pages');
-    //     createForm('isRead');
-    //         document.querySelector("#isRead").setAttribute('type', "checkbox"); 
-    //         document.querySelector("#form-box > form > ul:nth-child(4) > label").innerHTML = 'Have you already read this book?'
-
-        
-    //     formBox.appendChild(document.createElement('ul')); 
 
 
       
