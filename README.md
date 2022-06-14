ba72d4de4b9c176e64322000b1a5a40c88c14c3f
saskiabt <sskbinder@gmail.com>
2022-06-14 11:10:49 -0400

    added parcel

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 In this project I am creating a library app that takes user input for BOOK INFO, and creates a javascript object with it, storing it in the user's 'library' within the app. 
 
+Still to do 6/9: 
+Add delete buttons to each of the cards to remove them from display
+Add edit button to the cards to edit the input?
+Add checkbox to to card to check off if read or not
--- script.js
+++ script.js
@@ -1,30 +1,72 @@
 
-// global variables 
+// GLOBAL VARIABLES 
 let myLibrary = []; 
+const formWrapper = document.querySelector(".popup-form-wrapper"); 
+const formContainer = document.querySelector('#form-container'); 
+
+// display form on click of create book button
+const bookButton = document.getElementById("create-book-button"); 
+bookButton.addEventListener('click', () => {
+    if (formWrapper.style.display = "none") {
+        openForm();
+        
+    }
+
+    document.querySelector("#cancel-btn").addEventListener('click', () => { 
+        closeForm();
+        let blur = document.querySelector('.blur'); 
+        if (blur.classList.contains('active') == true) {
+            toggleBlur();
+        }
+        clearForm();
+    });
+});
+
+// Open and close the form functions: 
+function openForm() { 
+    toggleBlur();
+    formWrapper.style.display = "block"; 
+    // formWrapper.style.zIndex = "1"
+
+    bookButton.style.visibility = "hidden"; 
+    bookButton.style.padding = "0px";
+}
+function closeForm() { 
+    formWrapper.style.display = "none"; 
+    // formWrapper.style.zIndex = "-1"
+    bookButton.style.visibility = "visible";
+    bookButton.style.padding = "10px";
+    toggleBlur();
+    
+}
+
+function clearForm () { 
+    document.querySelector("#title").value = '';
+    document.querySelector("#author").value = '';
+    document.querySelector("#pages").value = '';
+    document.querySelector("#finished").checked = false;
+    document.querySelector("#not-finished").checked = false;
+    document.querySelector("#\\31 star").checked = false
+    document.querySelector("#\\32 star").checked = false; 
+    document.querySelector("#\\33 star").checked = false; 
+    document.querySelector("#\\34 star").checked = false; 
+    document.querySelector("#\\35 star").checked = false; 
+}
 
 
-// creates book object 
+// Create book object function: 
 function Book(title,author,pages,review,isRead) {
     // the constructor
-    this.title = `Title: ${title}`;
+    this.title = `${title}`;
     this.author = `Author: ${author}`; 
     this.pages = `Page Count: ${pages}`; 
     this.review = `My Rating: ${review}/5 Stars`; 
     this.isRead = isRead; 
 }
 
-Book.prototype.info = function() {
-    if (isRead === true) { 
-        return `${title} by ${author}, ${pages} pages, already read. `;
-    } else {
-        return `${title} by ${author}, ${pages} pages, not read yet `;
-    }
-}
-
-
+// Add book to myLibrary Array Function
 function addBookToLibrary() { 
     const title = document.querySelector("#title").value; 
-    console.log(title);
     const author = document.querySelector("#author").value;
     const pageCount = document.querySelector("#pages").value;
 
@@ -42,55 +84,23 @@ function addBookToLibrary() {
             rating = radio.value;
         } 
     }
-
-
-    console.log(rating);
-
-    const newBook = new Book(title,author,pageCount,rating,finishedCheck)
-    console.log(newBook); 
-    myLibrary.push(newBook); 
-}
-
-// display form on click of create book button
-const bookButton = document.getElementById("create-book-button"); 
-bookButton.addEventListener('click', () => {
-    openForm(); 
-
-    document.querySelector("#cancel-btn").addEventListener('click', () => { 
-        closeForm();
-        bookButton.style.visibility = "visible";
-        bookButton.style.padding = "10px";
-    });
-});
-
-//  functions to open and close the form; 
-
-function openForm() { 
-    document.getElementById('popup-form').style.display = "block"; 
-    bookButton.style.visibility = "hidden"; 
-    bookButton.style.padding = "0px";
-}
-function closeForm() { 
-    document.getElementById('popup-form').style.display = "none"; 
-    bookButton.style.visibility = "visible";
-    bookButton.style.padding = "10px";
-}
-
-function clearForm() { 
+    // if (title.value == '') { 
+    //     title.classList.add('error'); 
+    //     return; 
+    // } else if (author.value = '') { 
+    //     author.classList.add('error')
+    //     return; 
+    // } else if (pageCount.value = '') { 
+    //     pageCount.classList.add('error'); 
+    //     return;
+    // } else {
+        const newBook = new Book(title,author,pageCount,rating,finishedCheck)
+        myLibrary.push(newBook); 
 
 }
-   
 
-// Save inputs when submit button is pressed 
-const submitButton = document.querySelector("#submit-btn"); 
-submitButton.addEventListener('click', () => {
-   addBookToLibrary(); 
-   console.log(myLibrary); 
-   closeForm();
-   displayBooks(); 
-}); 
 
-// function to create div with book information 
+// Function to display "card" with book
 function displayBooks() { 
     const libraryWrapper = document.querySelector("#library-wrapper"); 
 
@@ -99,44 +109,87 @@ function displayBooks() {
     }
 
     for(let i=0; i<myLibrary.length; i++) { 
+
         const card = document.createElement('div'); 
         card.classList.add('card'); 
         libraryWrapper.appendChild(card); 
 
-        let ul = document.createElement('ul')
-        card.appendChild(ul); 
+        const front = document.createElement('div'); 
+        card.appendChild(front); 
+        front.classList.add('front'); 
+        front.textContent = myLibrary[i].title; 
 
-        let title= createListItem(myLibrary[i].title); 
-        ul.appendChild(title); 
+        const back = document.createElement('div'); 
+        card.appendChild(back); 
+        back.classList.add('back'); 
+        
+            let ul = document.createElement('ul')
+            back.appendChild(ul); 
 
-        let author = createListItem(myLibrary[i].author)
-        ul.appendChild(author); 
+            let author = createListItem(myLibrary[i].author)
+            ul.appendChild(author); 
 
-        let pages = createListItem(myLibrary[i].pages)
-        ul.appendChild(pages); 
+            let pages = createListItem(myLibrary[i].pages)
+            ul.appendChild(pages); 
 
-        let rating = createListItem(myLibrary[i].review)
-        ul.appendChild(rating); 
+            let rating = createListItem(myLibrary[i].review)
+            ul.appendChild(rating); 
 
-        let isRead = document.createElement('li'); 
-        ul.appendChild(isRead);
-        if (myLibrary[i].isRead == true) { 
-            isRead.textContent = "Finished"
-        } else {
-            isRead.textContent = "Not Finished"
-        }
+            let isRead = document.createElement('li'); 
+            ul.appendChild(isRead);
+            if (myLibrary[i].isRead == true) { 
+                isRead.textContent = "Finished"
+            } else {
+                isRead.textContent = "Not Finished"
+            }
+        const rBtn = document.createElement('button'); 
+        back.appendChild(rBtn); 
+        rBtn.classList.add("read-button"); 
+    }
+
+    // creates <li>text</li> element
+    function createListItem(text) { 
+        let li = document.createElement('li'); 
+        li.classList.add("card-info");
+        li.textContent = text
+        return li;
     }
 }
 
+// Save inputs and create new book card with book info when submit button is pressed: 
+const submitButton = document.querySelector("#submit-btn"); 
+submitButton.addEventListener('click', () => {
 
-// creates <li>text</li> element
-function createListItem(text) { 
-    let li = document.createElement('li'); 
-    li.classList.add("card-info");
-    li.textContent = text
-    return li;
+   submitCard(); 
+    clearForm(); 
+
+   function submitCard() { 
+        addBookToLibrary(); 
+        console.log(myLibrary); 
+        closeForm();
+        displayBooks(); 
+    }
+}); 
+
+function toggleBlur() { 
+    let blur = document.querySelector('.blur'); 
+    blur.classList.toggle('active')
 }
 
-      
-
-   
+// handler function for checking if 'checkbox' input element has been checked ; first set ele to checked and then run function to either clear or check boxes 
+function onClickHeader(ele) {
+    var checkboxes = document.getElementsByTagName('input');
+    if (ele.checked) {
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].type == 'checkbox') {
+                checkboxes[i].checked = true;
+            }
+        }
+    } else {
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].type == 'checkbox') {
+                checkboxes[i].checked = false;
+            }
+        }
+    }
+}
